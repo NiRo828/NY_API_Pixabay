d33870c548a959d6bb031db9792cb1ce3cab7a43
NiRo828 <111566330+NiRo828@users.noreply.github.com>
2024-08-31 21:59:44 +0300

    Backend: updated cache mechnism

--- domain/cache.js
+++ domain/cache.js
@@ -1,11 +1,24 @@
 const cache = {};
+const TTL = 60000; // in milliseconds (e.g., 60 seconds)
 
 function getFromCache(key) {
-    return cache[key];
+    const cached = cache[key];
+    if (!cached) return null;
+
+    const isExpired = (Date.now() - cached.timestamp) > TTL;
+    if (isExpired) {
+        delete cache[key];
+        return null;
+    }
+
+    return cached.data;
 }
 
 function setInCache(key, data) {
-    cache[key] = data;
+    cache[key] = {
+        data,
+        timestamp: Date.now(),
+    };
 }
 
 module.exports = {
