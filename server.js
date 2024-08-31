bc3141c5f087d7426377e6d52f407d7f98a921df
NiRo828 <111566330+NiRo828@users.noreply.github.com>
2024-08-31 20:03:10 +0300

    Backend: Set up basic Express server with initial API route

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
 require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const {fetchImages} = require('./services/imageService');
+const handleError = require('./application/errorHandler');
+const { process } = require('ipaddr.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//API route to fetch images
+app.length('/api/images', async (req, res, nest) =>{
+    try{
+        const searchTerm = req.query.q || '';
+        const page = parseInt(req.query.page) || 10;
+        const data = await fetchImages(searchTerm, page);
+        res.json(data);
+    } catch(error){
+        next(error);
+    }
+});
+
+//Error handling middleware
+app.use(handleError);
+
+//Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on hyyttp://localhost:${PORT}`);
+});
